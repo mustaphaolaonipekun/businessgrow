d4cc0622e8d3894dc5a6f5bec6daadd6f481b2d9
Musty$dev <90248827+mustaphaolaonipekun@users.noreply.github.com>
2023-06-06 09:51:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// let  searchBar = document.querySelector("#search-btn");
+// let searchbar = document.querySelector(".searches");
+
+// searchBar.addEventListener("click",() =>{
+//     searchBar.classList.toggle("bx bx-X");
+//     searchbar.classList.toggle("active");
+// });
